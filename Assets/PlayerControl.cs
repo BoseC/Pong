f39f3793325ecb4708dbed829d678b950802530f
Bosko Koloski <bosec99@gmail.com>
2016-11-05 11:57:54 +0100

    Second

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float PlayerSpeed = 1;
+    public Vector3 position;
+
+void Update () {
+        float yPosition = gameObject.transform.position.y + (Input.GetAxis("Vertical") * PlayerSpeed);
+        position = new Vector3(-10, Mathf.Clamp(yPosition, -4, 4),0);
+        gameObject.transform.position = position;	
+	}
+}
