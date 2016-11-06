99f7a40253be57359cde2d970b2fd382158e65d3
Bosko Koloski <bosec99@gmail.com>
2016-11-06 12:08:14 +0100

    Ball Added

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -2,14 +2,26 @@
 using System.Collections;
 
 public class Ball : MonoBehaviour {
+    private Rigidbody2D rb;
+    public float speed=4f;
+    void borders()
+    {
+        if (this.transform.position >= (17f,0,0))
+        {
 
-	// Use this for initialization
+        }
+
+    }
+    // Use this for initialization
 	void Start () {
-	
-	}
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(speed, speed);
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        
+
 	
 	}
 }
