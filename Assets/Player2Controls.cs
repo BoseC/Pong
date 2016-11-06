0fe6d8acc348a5bdc56d9870efdf98a74ef5645e
Bosko Koloski <bosec99@gmail.com>
2016-11-06 10:35:26 +0100

    Submit me

---
+++ Assets/Player2Controls.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Controls : MonoBehaviour {
+
+    private Rigidbody2D rb2;
+    public float speed = 5;
+    void movement()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))        {
+            rb2.velocity = new Vector2(0f, speed);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb2.velocity = new Vector2(0f, -speed);
+
+        }
+        else
+            rb2.velocity = new Vector2(0f, 0f);
+    }
+
+	// Use this for initialization
+	void Start () {
+    rb2 = this.GetComponent<Rigidbody2D>();
+}
+	
+	// Update is called once per frame
+	void Update () {
+    movement();
+	}
+
+}
--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -1,14 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class PlayerControl : MonoBehaviour {
+public class PlayerControl : MonoBehaviour
+{
 
-    public float PlayerSpeed = 1;
-    public Vector3 position;
+    private Rigidbody2D rb;
+    public float speed = 5f;
 
-void Update () {
-        float yPosition = gameObject.transform.position.y + (Input.GetAxis("Vertical") * PlayerSpeed);
-        position = new Vector3(-10, Mathf.Clamp(yPosition, -4, 4),0);
-        gameObject.transform.position = position;	
-	}
+    void movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(0f, speed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector2(0f, -speed);
+
+        }
+        else
+            rb.velocity = new Vector2(0f, 0f);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
 }
