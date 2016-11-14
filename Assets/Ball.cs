60535b2998de375a079e6855b631c03b41540b88
Bosko Koloski <bosec99@gmail.com>
2016-11-14 15:16:42 +0100

    Working Game, Added Score, Polished BallMovement

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -6,22 +6,38 @@ public class Ball : MonoBehaviour {
     public float speed=4f;
     void borders()
     {
-        if (this.transform.position >= (17f,0,0))
+        if (Mathf.Abs(this.transform.position.x) >= 10f)
         {
+            transform.position = new Vector3(0f, 0f, 0f);
+            StartCoroutine(Pause());
 
         }
-
-    }
+       
+     }
     // Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(speed, speed);
+        StartCoroutine(Pause());
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
+        borders();
 
 	
 	}
+    IEnumerator Pause()
+    {
+        int directionX = Random.Range(-1, 2);
+        int directionY = Random.Range(-1, 2);
+
+        if(directionX == 0)
+        {
+            directionX = 1;
+        }
+        rb.velocity = new Vector2(0f, 0f);
+    yield return new WaitForSeconds(2);
+        rb.velocity = new Vector2(6f * directionX,6f * directionY); 
+    }
 }
---
+++ Assets/Score.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public Text scoreboard;
+    public GameObject ball;
+    private int score_p1=0;
+    private int score_p2 =0;
+
+	// Use this for initialization
+
+    
+	void Start () {
+        ball = GameObject.Find("ball");
+	
+	}
+    void Result()
+    {
+        if (ball.transform.position.x >= 10f)
+        {
+            score_p1++;
+        }
+        if (ball.transform.position.x <= -10f)
+        {
+            score_p2++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Result();
+        scoreboard.text = score_p1.ToString() + " " + score_p2.ToString();
+
+
+    }
+
+}
